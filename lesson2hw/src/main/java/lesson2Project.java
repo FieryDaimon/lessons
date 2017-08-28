5399c60023c32a9e3a69262854f97dbd33160c74
PeaceDemon <mikuzev@gmail.com>
2017-08-28 17:56:52 +0300

    Add Task 2 and Task3

--- lesson2hw/src/main/java/lesson2Project.java
+++ lesson2hw/src/main/java/lesson2Project.java
@@ -17,35 +17,39 @@ public class lesson2Project {
         for (int i = 0; i < size; i++) {
             intArray[i] = scanner.nextInt();
         }
+        String _num = "";
+        while (!_num.equals("q")){
+            System.out.print("Введите номер задачи или 'q' для выхода из программы: ");
 
-        System.out.print("Введите номер задачи: ");
-
-        String number = scanner.next();
+            String number = scanner.next();
 
 
-        boolean r = false;
+            boolean r = false;
 
-        switch (number){
-            case "1":
-                r = task1SortArray(intArray);
-                break;
-            case "2":
-                //task2();
-                break;
-            case "3":
-                //task3();
-                break;
-            case "q":
-                //task3();
-                break;
-            default: System.out.println("Введите номер задачи (1, 2, 3). Для выхода введите 'q'.");
-        }
+            switch (number){
+                case "1":
+                    r = task1SortArray(intArray);
+                    break;
+                case "2":
+                    r = task2SearchMinAndMaxAndSum(intArray);
+                    break;
+                case "3":
+                    r = task3RepeatCount(intArray);
+                    break;
+                case "q":
+                    _num = number;
+                    System.out.println("Выход из программы");
+                    break;
+                default: System.out.println("Введите номер задачи (1, 2, 3). Для выхода введите 'q'.");
+            }
         /*if(number == 1) r = task1();
         else if (number == 2) r = task2();
         else System.out.println("Введите номер задачи (1, 2, 3)");*/
-        System.out.println("");
-        if (r == true) System.out.println("Задача выполнена.");
-        else System.out.println("Задача не выполнена.");
+            System.out.println("");
+            if (r == true) System.out.println(format("Задача %s выполнена", number));
+            else if (r == true && !_num.equals("q")) System.out.println("Задача не выполнена.");
+        }
+
         scanner.close();
     }
 
@@ -73,6 +77,72 @@ public class lesson2Project {
             System.out.print(format("%s ",array[i]));
         }
 
+        return true;
+    }
+
+    public static boolean task2SearchMinAndMaxAndSum(int[] array){
+        int max=array[0];
+        int min=array[0];
+        for (int i = 0; i<array.length;i++){
+
+            if(max<array[i]) max = array[i];
+            if(min>array[i]) min = array[i];
+        }
+        System.out.println(format("Максимальное значение в массиве: %s ",max));
+        System.out.println(format("Минимальное значение в массиве: %s ",min));
+        System.out.println(format("Сумма максимума и минимума: %s + %s = %s ",max, min,max+min));
+        return true;
+    }
+
+    public static boolean task3RepeatCount(int[] array){
+        int count = 0;
+        int element = 0;
+        int countRav = 0;
+        int countMax = 0;
+        int tmp=0;
+
+        boolean no_max = false;
+        //int[] ar = new int[10];
+
+        for (int k:array
+             ) {
+            tmp = 0;
+            for (int j = 0; j<array.length; j++){
+                if (k==array[j]) {
+                    tmp++;
+                }
+            }
+            if(tmp>count){
+                //no_max = false;
+                //countMax++;
+                element = k;
+                count = tmp;
+            }
+
+        }
+        /*for(int i = 0; i<array.length; i++){
+            int tmp=0;
+            for (int j = 0; j<array.length; j++){
+                if (array[i]==array[j]) {
+                    tmp++;
+                }
+            }
+
+            if(tmp>count){
+                //no_max = false;
+                countMax++;
+                element = array[i];
+                count = tmp;
+            }
+            else if (tmp==count) countRav++;
+        }*/
+        System.out.println(format("Наибольшее количество раз повторяется элемент '%s': %s повторений.",element, count));
+        //if (countMax>1) System.out.println(format("Наибольшее количество раз повторяется элемент '%s': %s повторений.",element, count));
+        //else if (countMax == countRav) System.out.println(format("Есть элементы, повторяющиеся одинаковое количество раз."));
+        //if (no_max){
+          //  System.out.println(format("Элементы повторяются одинаковое количество раз."));
+        //}
+        //else System.out.println(format("Наибольшее количество раз повторяется элемент '%s': %s повторений.",element, count));
         return true;
     }
 
